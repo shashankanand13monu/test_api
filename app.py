14a9d135bb2ebe332acb95c799af502aacc26cf7
shashankanand13monu <79691562+shashankanand13monu@users.noreply.github.com>
2021-11-04 21:39:37 +0530

    Added

--- app.py
+++ app.py
@@ -1,55 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 17 21:40:41 2020
-
-@author: win10
-"""
-
-# 1. Library imports
+from fastapi import FastAPI,UploadFile,File
+from numpy.core.defchararray import index
+from numpy.lib.type_check import imag
 import uvicorn
-from fastapi import FastAPI
-from BankNotes import BankNote
 import numpy as np
-import pickle
-import pandas as pd
-# 2. Create the app object
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+# import http 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
-pickle_in = open("classifier.pkl","rb")
-classifier=pickle.load(pickle_in)
 
-# 3. Index route, opens automatically on http://127.0.0.1:8000
-@app.get('/')
-def index():
-    return {'message': 'Hello, World'}
+# var port = process.env.PORT || 8000;
 
-# 4. Route with a single parameter, returns the parameter within a message
-#    Located at: http://127.0.0.1:8000/AnyNameHere
-@app.get('/{name}')
-def get_name(name: str):
-    return {'Welcome To Krish Youtube Channel': f'{name}'}
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://fronend-web-app.herokuapp.com/",
+    "https://fronend-web-app.herokuapp.com",
+    "http://127.0.0.1/"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# 3. Expose the prediction functionality, make a prediction from the passed
-#    JSON data and return the predicted Bank Note with the confidence
-@app.post('/predict')
-def predict_banknote(data:BankNote):
-    data = data.dict()
-    variance=data['variance']
-    skewness=data['skewness']
-    curtosis=data['curtosis']
-    entropy=data['entropy']
-   # print(classifier.predict([[variance,skewness,curtosis,entropy]]))
-    prediction = classifier.predict([[variance,skewness,curtosis,entropy]])
-    if(prediction[0]>0.5):
-        prediction="Fake note"
-    else:
-        prediction="Its a Bank note"
-    return {
-        'prediction': prediction
-    }
 
-# 5. Run the API with uvicorn
-#    Will run on http://127.0.0.1:8000
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+MODEL= tf.keras.models.load_model("saved_model/1")
+CLASS_NAMES =["Early Bright","Healthy","Late Blight"]
+
+
+
+@app.get("/ping")
+async def ping():
+    return "Sup Brother"
+
+def read_file_as_image(data) -> np.ndarray:
+    image= np.array(Image.open(BytesIO(data)))
+    return image
     
-#uvicorn app:app --reload
+
+@app.post("/predict")
+async def predict(file: UploadFile= File(...)):
+    image= read_file_as_image(await file.read())
+    img_batch= np.expand_dims(image,axis=0)
+    predictions= MODEL.predict(img_batch)  
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence= np.max(predictions[0])
+    print(predicted_class,confidence)
+    return{
+        "class": predicted_class,
+        "confidence":float(confidence)}
+    
+
+
+if __name__== "__main__":
+    uvicorn.run(app,debug=True)
